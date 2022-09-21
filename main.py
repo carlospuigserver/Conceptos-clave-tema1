642abae85d9970d4c2c173919766f625a24ac5bf
carlospuigserveer <91721643+carlospuigserver@users.noreply.github.com>
2022-09-21 15:17:06 +0000

    Cuadrante

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 #Crea una clase llamada Punto con sus coordenadas X e Y.
 
 class Punto:
-    def __init__(self, coord_X, coord_Y):
-      self.coord_X = coord_X
-      self.coord_Y = coord_Y
+    def __init__(self, coordx, coordy):
+      self.coordx = coordy
+      self.coordx = coordy
 
 #Método string para que al imprimir por pantalla un punto aparezca en formato (X,Y)
 
 def __str__(self):
-        return  "({}, {})".format(self.coord_X, self.coord_Y)
+        return  "({}, {})".format(self.coordx, self.coordy)
+
+
+#Añade un método llamado cuadrante que indique a qué cuadrante pertenece el punto, teniendo en cuenta que si X == 0 e Y != 0 se sitúa sobre el eje Y, si X != 0 e Y == 0 se sitúa sobre el eje X y si X == 0 e Y == 0 está sobre el origen.
+
+def cuadrante(self):
+  
+  
+  
+  if self.coordenada_X != 0 and self.coordenada_Y == 0:
+    print("{} está en el eje X".format(self))
+  
+  elif self.coordenada_X == 0 and self.coordenada_Y != 0:
+    print("{} se sitúa sobre el eje Y".format(self))
+  
+  elif self.coordx>0 and self.coordy>0:
+    print("{} está en el primer cuadrante".format(self))
+  
+  elif self.coordx>0 and self.coordy<0:
+    print("{} está en el cuarto cuadrante".format(self))
+  
+  elif self.coordx<0 and self.coordy<0:
+    print("{} está en el tercer cuadrante".format(self))
+  
+  elif self.coordx<0 and self.coordy>0:
+    print("{} está en el segundo cuadrante".format(self))
+  
+  else: 
+    print("{} está en el origen".format(self))
+    
+    
+            
 
 
